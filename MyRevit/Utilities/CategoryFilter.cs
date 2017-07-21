8da9814536a98e5547f42a7d283e6f496a4e08e0
vlong638 <vlong638@163.com>
2017-07-21 17:14:24 +0800

    增加"梁齐板"功能 实现 板分层换算法

---
+++ MyRevit/Utilities/CategoryFilter.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB;
+
+namespace MyRevit.MyTests.Utilities
+{
+    /// <summary>
+    /// 管道 选择过滤器
+    /// </summary>
+    public class CategoryFilter : ISelectionFilter
+    {
+        BuiltInCategory TargetCategory { set; get; }
+
+        public CategoryFilter(BuiltInCategory category)
+        {
+            TargetCategory = category;
+        }
+
+        public bool AllowElement(Element element)
+        {
+            var document = element.Document;
+            var category = Category.GetCategory(document, TargetCategory);
+            return element.Category.Id == category.Id;
+        }
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
---
+++ MyRevit/Utilities/MessageHelper.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRevit.Utilities
+{
+    class MessageHelper
+    {
+        public  static void TaskDialogShow(string message,string title= "警告")
+        {
+            TaskDialog.Show(title, message);
+        }
+    }
+}
