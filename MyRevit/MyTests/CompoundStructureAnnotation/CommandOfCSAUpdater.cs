e6084403e74e70005daf1e7907a005a6b01330bf
vlong638 <vlong638@163.com>
2017-11-03 17:38:28 +0800

    修正测试用的Updater注册

--- MyRevit/MyTests/CompoundStructureAnnotation/CommandOfCSAUpdater.cs
+++ MyRevit/MyTests/CompoundStructureAnnotation/CommandOfCSAUpdater.cs
@@ -26,16 +26,26 @@ namespace MyRevit.MyTests.BeamAlignToFloor
             var editUpdater = new CompoundStructureAnnotationUpdater_Edit(new AddInId(new Guid("B593F2C4-F38C-41D7-AE2C-369BB0149D90")));
             var updaterInfo = UpdaterRegistry.GetRegisteredUpdaterInfos().FirstOrDefault(c => c.UpdaterName == editUpdater.GetUpdaterName());
             if (updaterInfo == null)
-            {
-                UpdaterRegistry.RegisterUpdater(editUpdater, true);
-                UpdaterRegistry.AddTrigger(editUpdater.GetUpdaterId(), new LogicalOrFilter(new List<ElementFilter>() {
+                UpdaterRegistry.UnregisterUpdater(editUpdater.GetUpdaterId());
+            UpdaterRegistry.RegisterUpdater(editUpdater, true);
+            UpdaterRegistry.AddTrigger(editUpdater.GetUpdaterId(), new LogicalOrFilter(new List<ElementFilter>() {
                     new ElementCategoryFilter(BuiltInCategory.OST_Walls),//墙
                     new ElementCategoryFilter(BuiltInCategory.OST_Floors),//板
                     new ElementCategoryFilter(BuiltInCategory.OST_Roofs),//屋顶
                     new ElementCategoryFilter(BuiltInCategory.OST_TextNotes),//文本
                 })
-                , Element.GetChangeTypeAny());
-            }
+            , Element.GetChangeTypeAny());
+            //if (updaterInfo == null)
+            //{
+            //    UpdaterRegistry.RegisterUpdater(editUpdater, true);
+            //    UpdaterRegistry.AddTrigger(editUpdater.GetUpdaterId(), new LogicalOrFilter(new List<ElementFilter>() {
+            //        new ElementCategoryFilter(BuiltInCategory.OST_Walls),//墙
+            //        new ElementCategoryFilter(BuiltInCategory.OST_Floors),//板
+            //        new ElementCategoryFilter(BuiltInCategory.OST_Roofs),//屋顶
+            //        new ElementCategoryFilter(BuiltInCategory.OST_TextNotes),//文本
+            //    })
+            //    , Element.GetChangeTypeAny());
+            //}
             var deleteUpdater = new CompoundStructureAnnotationUpdater_Delete(new AddInId(new Guid("B593F2C4-F38C-41D7-AE2C-369BB0149D90")));
             updaterInfo = UpdaterRegistry.GetRegisteredUpdaterInfos().FirstOrDefault(c => c.UpdaterName == deleteUpdater.GetUpdaterName());
             if (updaterInfo == null)
