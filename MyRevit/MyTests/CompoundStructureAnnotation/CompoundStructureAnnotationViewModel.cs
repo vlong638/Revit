3a9c84c4c87e652e44b66d2f638653c5981c140d
vlong638 <vlong638@163.com>
2017-11-03 17:28:45 +0800

    需要注意 文本只有一项的情况需要另作处理

--- MyRevit/MyTests/CompoundStructureAnnotation/CompoundStructureAnnotationViewModel.cs
+++ MyRevit/MyTests/CompoundStructureAnnotation/CompoundStructureAnnotationViewModel.cs
@@ -976,33 +976,36 @@ namespace MyRevit.MyTests.CompoundStructureAnnotation
             var texts = model.FetchTextsFromCompoundStructure(doc, compoundStructure);//获取文本数据
             if (texts.Count == 0)
                 return;
-            model.TargetId = element.Id;//主体
-            var lineFamilySymbol = VLConstraints.GetMultipleTagSymbol(doc);//获取线标注类型 
-            var line = doc.Create.NewFamilyInstance(new XYZ(0, 0, 0), lineFamilySymbol, doc.ActiveView);//生成 线
-            model.CalculateLocations(element, line, offset);//计算内容定位
-            var lineLocation = model.LineLocation;
-            var textLocations = model.TextLocations;
-            ElementTransformUtils.MoveElement(doc, line.Id, lineLocation);//线定位
-            LocationPoint locationPoint = line.Location as LocationPoint;//线 旋转处理
-            locationPoint.RotateByXY(lineLocation, model.VerticalVector);
-            model.LineId = line.Id;
-            //Location
-            //Height
-            //LineWidth
-            //TextHeight+TextSpan
-            model.UpdateLineParameters(line, model.LineHeight, model.LineWidth, model.LineSpace, model.Texts.Count());//线参数设置
-            //文本生成
-            List<TextNote> textNotes = new List<TextNote>();
-            foreach (var text in model.Texts)//生成 文本
+            if (texts.Count==1)
             {
-                var textLocation = model.TextLocations[model.Texts.IndexOf(text)];
-                var textNote = TextNote.Create(doc, doc.ActiveView.Id, textLocation, text, model.TextNoteTypeElementId);
-                textNotes.Add(textNote);
-                textNote.Location.RotateByXY(textLocation, model.VerticalVector);
+                //简单标注
             }
-            model.TextNoteIds = textNotes.Select(c => c.Id).ToList();
-            //测试用
-            //GraphicsDisplayerManager.Display(@"E:\WorkingSpace\Outputs\Images\1023结构做法标注.png", lines, Model.TextLocations);
+            else
+            {
+                model.TargetId = element.Id;//主体
+                var lineFamilySymbol = VLConstraints.GetMultipleTagSymbol(doc);//获取线标注类型 
+                var line = doc.Create.NewFamilyInstance(new XYZ(0, 0, 0), lineFamilySymbol, doc.ActiveView);//生成 线
+                model.CalculateLocations(element, line, offset);//计算内容定位
+                var lineLocation = model.LineLocation;
+                var textLocations = model.TextLocations;
+                ElementTransformUtils.MoveElement(doc, line.Id, lineLocation);//线定位
+                LocationPoint locationPoint = line.Location as LocationPoint;//线 旋转处理
+                locationPoint.RotateByXY(lineLocation, model.VerticalVector);
+                model.LineId = line.Id;
+                model.UpdateLineParameters(line, model.LineHeight, model.LineWidth, model.LineSpace, model.Texts.Count());//线参数设置
+                                                                                                                          //文本生成
+                List<TextNote> textNotes = new List<TextNote>();
+                foreach (var text in model.Texts)//生成 文本
+                {
+                    var textLocation = model.TextLocations[model.Texts.IndexOf(text)];
+                    var textNote = TextNote.Create(doc, doc.ActiveView.Id, textLocation, text, model.TextNoteTypeElementId);
+                    textNotes.Add(textNote);
+                    textNote.Location.RotateByXY(textLocation, model.VerticalVector);
+                }
+                model.TextNoteIds = textNotes.Select(c => c.Id).ToList();
+                //测试用
+                //GraphicsDisplayerManager.Display(@"E:\WorkingSpace\Outputs\Images\1023结构做法标注.png", lines, Model.TextLocations);
+            }
         }
 
         internal void Regenerate(Document doc, CSAModelForFamilyInstance model, Element target)
